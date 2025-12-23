d582ac4c64b298687b04ac32adf7b9a189719169
Saniya Potdar <saniyapotdar03@gmail.com>
2025-12-23 23:14:36 +0530

    Day 7 completed

---
+++ Day7/demo2.py
@@ -0,0 +1,25 @@
+from langchain_openai import OpenAIEmbeddings
+import numpy as np
+
+def cosine_similarity(a,b):
+    return np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+embed_model = OpenAIEmbeddings(
+    model = "text-embedding-nomic-embed-text-v1.5",
+    base_url = "http://localhost:1234/v1",
+    api_key = "dummy-token",
+    check_embedding_ctx_length= False
+)
+
+sentences = [
+    "I love tennis",
+    "My favorite player is Messi",
+    "Roger federer talks german"
+]
+embeddings = embed_model.embed_documents(sentences)
+
+for embed_vect in embeddings:
+    print("Len:", len(embed_vect)," -->",embed_vect[:4])
+
+print("Sentence 1 & 2 similarity:", cosine_similarity(embeddings[0], embeddings[1]))
+print("Sentence 1 & 3 similarity:", cosine_similarity(embeddings[0], embeddings[2]))
